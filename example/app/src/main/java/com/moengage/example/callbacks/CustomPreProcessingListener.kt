90f7b7dabacf52011329db4f948ba9181845a7ac
umangmoe <umang@moengage.com>
2023-07-18 17:39:19 +0530

    initialise sdk in click callback

--- example/app/src/main/java/com/moengage/example/callbacks/CustomPreProcessingListener.kt
+++ example/app/src/main/java/com/moengage/example/callbacks/CustomPreProcessingListener.kt
@@ -1,13 +1,13 @@
 package com.moengage.example.callbacks
 
 import com.moengage.core.listeners.IntentPreProcessingListener
+import com.moengage.example.MoEngageHandler
 import logcat.logcat
 
 class CustomPreProcessingListener: IntentPreProcessingListener {
 
     override fun onIntentReceived() {
         logcat { "onIntentReceived(): Trying to process intent before SDK Initialization" }
-
-        // Initialize SDK here to process the intent
+        MoEngageHandler().initialiseIfRequired()
     }
 }
