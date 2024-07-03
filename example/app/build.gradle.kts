4983a32e9abd5c1d00a83100cfe61827ba2a895d
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2024-07-03 14:21:55 +0530

    updating android-dependency-catalog to 4.3.0

--- example/app/build.gradle.kts
+++ example/app/build.gradle.kts
@@ -47,18 +47,18 @@ dependencies {
     // logging library used only for demonstration, not required by the SDK.
     implementation("com.squareup.logcat:logcat:0.1")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-process:2.7.0")
-    implementation("com.google.firebase:firebase-messaging:23.4.1")
+    implementation("com.google.firebase:firebase-messaging:24.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 apply(plugin = "com.google.gms.google-services")
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,14 +1,14 @@
 buildscript {
   dependencies {
-    classpath("com.android.tools.build:gradle:8.2.2")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    classpath("com.android.tools.build:gradle:8.4.0")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
   }
 }
 
 plugins {
-  id("com.android.application") version "8.2.2" apply false
-  id("com.android.library") version "8.2.2" apply false
-  id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+  id("com.android.application") version "8.4.0" apply false
+  id("com.android.library") version "8.4.0" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.23" apply false
   id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
